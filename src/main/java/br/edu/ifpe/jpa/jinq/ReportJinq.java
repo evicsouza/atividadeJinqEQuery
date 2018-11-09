e47cfa817de952f28e9d401b953872b90a397a5d
PRONATEC PRONATEC <pronatec@LABS.GARANHUNS.IFPE>
2018-11-09 12:12:56 -0300

    Exerc 2 de ambas atividades

---
+++ src/main/java/br/edu/ifpe/jpa/jinq/ReportJinq.java
@@ -0,0 +1,48 @@
+package br.edu.ifpe.jpa.jinq;
+
+import java.util.Date;
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+
+import br.edu.ifpe.jpa.IReportGenerator;
+import br.edu.ifpe.jpa.entities.Account;
+import br.edu.ifpe.jpa.entities.Client;
+
+public class ReportJinq implements IReportGenerator {
+
+	static EntityManagerHelper helper = EntityManagerHelper.getInstance();
+
+	@Override
+	public List<String> getClientNames(Date initialDate, Date finalDate) {
+		return helper.execute(Client.class, streams ->
+		streams
+		.where(client -> client.getBirthDate().after(initialDate))
+		.where(client -> client.getBirthDate().before(finalDate))
+		.select(Client::getName)
+		.sortedBy(client -> client)
+		.toList()
+				); 
+	}
+
+	@Override
+	public double getClientTotalCash(String email) {		
+		return helper.execute(Account.class, streams ->
+		streams
+		.where(account -> account.getClient().getEmail().equals(email))
+		.sumDouble(account -> account.getBalance())	
+
+				);
+
+	}
+
+	@Override
+	public List<String> getBestClientsEmails(String agency, int rankingSize) {
+		return helper.execute(Account.class, streams ->
+			streams
+				.where(account -> account.getAgency().equals(agency))
+				.sumDouble(account -> account.getBalance())
+				
+		);
+
+	}
+}
---
+++ src/main/java/br/edu/ifpe/jpa/querydsl/ReportQuerydsl.java
@@ -0,0 +1,47 @@
+package br.edu.ifpe.jpa.querydsl;
+
+import java.util.Date;
+import java.util.List;
+
+import br.edu.ifpe.jpa.IReportGenerator;
+import br.edu.ifpe.jpa.entities.QAccount;
+import br.edu.ifpe.jpa.entities.QClient;
+
+public class ReportQuerydsl implements IReportGenerator {
+
+	static EntityManagerHelper helper = EntityManagerHelper.getInstance();
+
+	@Override
+	public List<String> getClientNames(Date initialDate, Date finalDate) {
+		QClient client = QClient.client;
+
+		return helper.execute(query ->
+		query
+		.select(client.name)
+		.from(client)
+		.where(client.birthDate.after(initialDate).and(client.birthDate.before(finalDate)))
+		.orderBy(client.birthDate.asc())
+		.fetch()
+				);
+	}
+
+	@Override
+	public double getClientTotalCash(String email) {
+		QAccount account = QAccount.account;
+
+		return helper.execute(query ->
+		query
+			.select(account.balance.sum())
+			.where(account.client.email.eq(email))
+			.groupBy(account.balance)
+			.fetchOne()
+		);
+	}
+
+
+
+	@Override
+	public List<String> getBestClientsEmails(String agency, int rankingSize) {
+		throw new UnsupportedOperationException("Not implemented yet.");
+	}
+}
